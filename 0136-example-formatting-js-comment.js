d1b000fcfac24874e406df59b0ba775e3db2f394
ahw <andrewhallagan@gmail.com>
2013-04-21 12:04:15 -0700

    Addeding some example files.

---
+++ 0136-example-formatting-js-comment.js
@@ -0,0 +1,11 @@
+/**
+ * Here is the description of the function. I can keep writing stuff or use gqj to format this comment.
+ */
+function foo() {
+
+    // Here is an inline comment explaining something about something. It's very long but can easily be formatted with gqj.
+    // This next part is on another line.
+    // And so is this.
+    // gq{motion will figure out how to format the entire thing.
+    return true;
+}
